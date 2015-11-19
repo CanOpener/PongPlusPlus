9d50c45905670fa86466860500f22228f3412843
Mladen Kajic <mladenkajic96@gmail.com>
2015-11-19 13:27:05 +0000

    Connection Management Functionality

--- connection/connection.go
+++ connection/connection.go
@@ -1,8 +1,11 @@
 package connection
 
 import (
+	"github.com/satori/go.uuid"
 	"net"
 )
+
+var AllConnections []*Connection = make([]*Connection, 0, 100)
 
 type Connection struct {
 	Registered        bool
@@ -14,13 +17,31 @@ type Connection struct {
 	Socket            net.Conn
 }
 
-func NewConnection(conn net.Conn) Connection {
-	return Connection{
+func NewConnection(conn net.Conn) *Connection {
+	newConn := &Connection{
+		Alias:             uuid.NewV4().String(),
 		Registered:        false,
 		ReaderListening:   false,
 		WriterListening:   false,
 		IncommingMessages: make(chan []byte, 100),
 		OutgoingMessages:  make(chan []byte, 100),
 		Socket:            conn,
+	}
+
+	AddConnection(newConn)
+	return newConn
+}
+
+func AddConnection(conn *Connection) {
+	AllConnections = append(AllConnections, conn)
+}
+
+func RemoveConnection(conn *Connection) {
+	var i int
+	for i = 0; i < len(AllConnections); i++ {
+		if AllConnections[i].Alias == conn.Alias {
+			AllConnections = append(AllConnections[:i], AllConnections[i+1:]...)
+			return
+		}
 	}
 }
