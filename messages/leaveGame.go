69240c8258b98c1dd3b2557a23058b1983432bf5
Mladen Kajic <mladenkajic96@gmail.com>
2015-11-22 22:17:01 +0000

    Commented leaveGame.go and messages.go

--- messages/leaveGame.go
+++ messages/leaveGame.go
@@ -1,21 +1,27 @@
 package messages
 
+// LeaveGameMessage is a structure representing a Leave game message
 type LeaveGameMessage struct {
 	MessageType uint8
 }
 
+// NewLeaveGameMessage returns an instance of LeaveGameMessage
 func NewLeaveGameMessage() LeaveGameMessage {
 	return LeaveGameMessage{
 		MessageType: TypeLeaveGame,
 	}
 }
 
+// NewLeaveGameMessageFromBytes returns an instance of LeaveGameMessage
+// from a slice of bytes
 func NewLeaveGameMessageFromBytes(messageBytes []byte) LeaveGameMessage {
 	return LeaveGameMessage{
 		MessageType: uint8(messageBytes[0]),
 	}
 }
 
+// Bytes returns a slice of bytes representing a LeaveGameMessage
+// which can be sent through a connection
 func (ms *LeaveGameMessage) Bytes() []byte {
 	b := make([]byte, 1)
 	b[0] = byte(ms.MessageType)
--- messages/messages.go
+++ messages/messages.go
@@ -1,5 +1,6 @@
 package messages
 
+// Message Types
 const (
 	TypeRequestAlias uint8 = 1 + iota
 	TypeAliasApproved
@@ -19,9 +20,11 @@ const (
 )
 
 const (
+	// NullTerm represents a null terminator byte for comparrison reasons
 	NullTerm byte = byte('\000')
 )
 
+// GetMessageType returns the message type from a slice of bytes
 func GetMessageType(message []byte) uint8 {
 	return uint8(message[0])
 }
