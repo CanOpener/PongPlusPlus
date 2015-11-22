da2b9fc169cdd2b2f81c4402cf0928787af2d094
Mladen Kajic <mladenkajic96@gmail.com>
2015-11-22 22:00:46 +0000

    Commented AliasApproved.go

--- messages/aliasApproved.go
+++ messages/aliasApproved.go
@@ -1,21 +1,28 @@
 package messages
 
+// AliasApprovedMessage is the struct which describes an Alias approved message
 type AliasApprovedMessage struct {
+	//MessageType is the message type
 	MessageType uint8
 }
 
+// NewAliasApprovedMessage returns an instance of AliasApprovedMessage
 func NewAliasApprovedMessage() AliasApprovedMessage {
 	return AliasApprovedMessage{
 		MessageType: TypeAliasApproved,
 	}
 }
 
+// NewAliasApprovedMessageFromBytes returns an instance of AliasApprovedMessage
+// Based on a given slice of bytes
 func NewAliasApprovedMessageFromBytes(messageBytes []byte) AliasApprovedMessage {
 	return AliasApprovedMessage{
 		MessageType: uint8(messageBytes[0]),
 	}
 }
 
+// Bytes returns a slice of bytes representing an AliasApprovedMessage
+// which can be sent through a connection
 func (ms *AliasApprovedMessage) Bytes() []byte {
 	b := make([]byte, 1)
 	b[0] = byte(ms.MessageType)
