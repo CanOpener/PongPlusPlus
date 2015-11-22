f955768daa6c1f7c873491d97023ca81858dc6ac
Mladen Kajic <mladenkajic96@gmail.com>
2015-11-22 22:30:00 +0000

    Finished commenting messages package

--- messages/requestGameList.go
+++ messages/requestGameList.go
@@ -1,21 +1,27 @@
 package messages
 
+// RequestGameListMessage is a structure representing a Request game list message
 type RequestGameListMessage struct {
 	MessageType uint8
 }
 
+// NewRequestGameListMessage returns an instance of RequestGameListMessage
 func NewRequestGameListMessage() RequestGameListMessage {
 	return RequestGameListMessage{
 		MessageType: TypeRequestGameList,
 	}
 }
 
+// NewRequestGameListMessageFromBytes returns an instance of RequestGameListMessage
+// based on a slice of bytes
 func NewRequestGameListMessageFromBytes(messageBytes []byte) RequestGameListMessage {
 	return RequestGameListMessage{
 		MessageType: uint8(messageBytes[0]),
 	}
 }
 
+// Bytes returns a slice of bytes representing a RequestGameListMessage
+// which can be sent through a connection
 func (ms *RequestGameListMessage) Bytes() []byte {
 	messageBytes := make([]byte, 1)
 	messageBytes[0] = byte(ms.MessageType)
