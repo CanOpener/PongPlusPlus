c7570e5b9aff86b9e37dbfed576d04a351d7fc22
Mladen Kajic <mladenkajic96@gmail.com>
2015-12-22 01:09:56 +0000

    thread safe deque

--- game/game.cpp
+++ game/game.cpp
@@ -1,5 +1,25 @@
 #include <iostream>
+#include <mutex>
+#include <deque>
 using namespace std;
+
+typedef unsigned char BYTE
+
+// thread safe queuing
+mutex mu;
+deque<BYTE*> callQueue;
+
+void add(BYTE* bytes) {
+    lock_guard<mutex> locker(mu);
+    callQueue.push_back(bytes);
+}
+deque<BYTE*> getAll() {
+    lock_guard<mutex> locker(mu);
+    deque<BYTE*> ret = callQueue;
+    callQueue.clear();
+    return ret;
+}
+
 
 int main() {
     cout << "Hello World!" << endl;
