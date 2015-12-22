654ff37f50c6356fa9681b8e81887e2b76510f92
Mladen Kajic <mladenkajic96@gmail.com>
2015-12-22 15:15:38 +0000

    No more hardcoding, primitive flags available now

--- main.go
+++ main.go
@@ -1,15 +1,30 @@
 package main
 
 import (
+	"flag"
 	"github.com/canopener/PongPlusPlus-Server/connection"
 	"github.com/canopener/serverlog"
 	"net"
+	"os"
+	"strconv"
 )
 
 func main() {
-	serverlog.Init(true, true, 7, "/home/mladen/Desktop/")
-	serverlog.Startup("Server listening on localhost:3000")
-	listener, err := net.Listen("tcp", "localhost:3000")
+	help := flag.Bool("h", false, "Display this help message")
+	consoleLog := flag.Bool("C", false, "Allow logging to the console, default wont log")
+	fileLog := flag.String("L", "", "Specify directory for logging logfiles, default wont log")
+	logcount := flag.Int("N", -1, "Specify maximum logfiles in directory, default no limit")
+	portno := flag.Int("PORT", 3000, "Specify the port on which the server should listen, default 3000")
+	flag.Parse()
+
+	if *help {
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+
+	serverlog.Init(*consoleLog, (*fileLog != ""), *logcount, *fileLog)
+	serverlog.Startup("Server listening on localhost:", *portno)
+	listener, err := net.Listen("tcp", ":"+strconv.Itoa(*portno))
 	if err != nil {
 		serverlog.Fatal(err)
 	}
